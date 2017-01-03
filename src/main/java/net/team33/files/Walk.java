011abffd1607de77ca0aab6805e9dd0e06112cc2
Andi Kluge <andi.kluge@mobi4friends.de>
2017-01-03 09:34:10 +0100

    refactored: Course -> Walk

--- src/main/java/net/team33/files/Walk.java
+++ src/main/java/net/team33/files/Walk.java
@@ -6,13 +6,41 @@ import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.LinkOption;
 import java.nio.file.Path;
+import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
 
-public class Walk {
+public class Walk implements Runnable {
+
+    private static final ExceptionHandler DEFAULT_HANDLER = (path, caught) -> {
+        throw new IllegalArgumentException("Problem in <" + path + ">", caught);
+    };
+    private static final Predicate DEFAULT_PREDICATE = path -> true;
+
+    private final Catching catching;
+    private final List<Consumer> consumers = new LinkedList<>();
+
+    private Walk(final Catching catching) {
+        this.catching = catching;
+    }
 
     public static Through through(final Path path) {
         return new Through(path);
+    }
+
+    public final Walk apply(final Consumer consumer) {
+        consumers.add(consumer);
+        return this;
+    }
+
+    public final When when(final Predicate predicate) {
+        return new When(this, predicate);
+    }
+
+    @Override
+    public final void run() {
+        new Action(consumers, catching.handler, catching.recursion.predicate, catching.recursion.options.options)
+                .walk(catching.recursion.options.path);
     }
 
     @FunctionalInterface
@@ -37,20 +65,28 @@ public class Walk {
             this.path = path;
         }
 
-        public final When when(final Predicate predicate) {
-            return recursive(path -> true).when(predicate);
+        public final Options options(final LinkOption... options) {
+            return new Options(this, options);
         }
 
-        public final Apply apply(final Consumer consumer) {
-            return recursive(path -> true).apply(consumer);
-        }
-
-        public final Recursive recursive(final Predicate predicate) {
+        public final Recursion recursive(final Predicate predicate) {
             return options(LinkOption.NOFOLLOW_LINKS).recursive(predicate);
         }
 
-        public final Options options(final LinkOption... options) {
-            return new Options(path, options);
+        public final Catching catching(final ExceptionHandler handler) {
+            return recursive(DEFAULT_PREDICATE).catching(handler);
+        }
+
+        public final When when(final Predicate predicate) {
+            return catching(DEFAULT_HANDLER).when(predicate);
+        }
+
+        public final Walk apply(final Consumer consumer) {
+            return catching(DEFAULT_HANDLER).apply(consumer);
+        }
+
+        public final <C extends Collection<? super Path>> C to(final C target) {
+            return catching(DEFAULT_HANDLER).to(target);
         }
     }
 
@@ -58,88 +94,110 @@ public class Walk {
         private final Path path;
         private final LinkOption[] options;
 
-        private Options(final Path path, final LinkOption[] options) {
-            this.path = path;
+        private Options(final Through through, final LinkOption[] options) {
+            this.path = through.path;
             this.options = options;
         }
 
-        public final Recursive recursive(final Predicate predicate) {
-            return new Recursive(this, predicate);
+        public final Recursion recursive(final Predicate predicate) {
+            return new Recursion(this, predicate);
+        }
+
+        public final Catching catching(final ExceptionHandler handler) {
+            return recursive(DEFAULT_PREDICATE).catching(handler);
+        }
+
+        public final When when(final Predicate predicate) {
+            return catching(DEFAULT_HANDLER).when(predicate);
+        }
+
+        public final Walk apply(final Consumer consumer) {
+            return catching(DEFAULT_HANDLER).apply(consumer);
+        }
+
+        public final <C extends Collection<? super Path>> C to(final C target) {
+            return catching(DEFAULT_HANDLER).to(target);
         }
     }
 
-    public static class Recursive {
+    public static class Recursion {
         private final Options options;
         private final Predicate predicate;
 
-        private Recursive(final Options options, final Predicate predicate) {
+        private Recursion(final Options options, final Predicate predicate) {
             this.options = options;
             this.predicate = predicate;
         }
 
-        public final Apply apply(final Consumer consumer) {
-            return new Apply(this).apply(consumer);
+        public final Catching catching(final ExceptionHandler handler) {
+            return new Catching(this, handler);
         }
 
-        public When when(final Predicate predicate) {
-            return new When(new Apply(this), predicate);
+        public final When when(final Predicate predicate) {
+            return catching(DEFAULT_HANDLER).when(predicate);
+        }
+
+        public final Walk apply(final Consumer consumer) {
+            return catching(DEFAULT_HANDLER).apply(consumer);
+        }
+
+        public final <C extends Collection<? super Path>> C to(final C target) {
+            return catching(DEFAULT_HANDLER).to(target);
+        }
+    }
+
+    public static class Catching {
+        private final Recursion recursion;
+        private final ExceptionHandler handler;
+
+        private Catching(final Recursion recursion, final ExceptionHandler handler) {
+            this.recursion = recursion;
+            this.handler = handler;
+        }
+
+        public final Walk apply(final Consumer consumer) {
+            return new Walk(this).apply(consumer);
+        }
+
+        public final When when(final Predicate predicate) {
+            return new When(new Walk(this), predicate);
+        }
+
+        public final <C extends Collection<? super Path>> C to(final C target) {
+            apply(target::add).run();
+            return target;
         }
     }
 
     public static class When {
-        private final Apply apply;
+        private final Walk walk;
         private final Predicate predicate;
 
-        private When(final Apply apply, final Predicate predicate) {
-            this.apply = apply;
+        private When(final Walk walk, final Predicate predicate) {
+            this.walk = walk;
             this.predicate = predicate;
         }
 
-        public final Apply apply(final Consumer consumer) {
-            return apply.apply(path -> {
+        public Walk apply(final Consumer consumer) {
+            return walk.apply(path -> {
                 if (predicate.test(path)) {
                     consumer.accept(path);
                 }
             });
         }
-    }
 
-    public static class Apply {
-        private final Recursive recursive;
-        private final List<Consumer> consumers = new LinkedList<>();
-
-        public Apply(final Recursive recursive) {
-            this.recursive = recursive;
-        }
-
-        public final When when(final Predicate predicate) {
-            return recursive.when(predicate);
-        }
-
-        public final Apply apply(final Consumer consumer) {
-            this.consumers.add(consumer);
-            return this;
-        }
-
-        public final void catching(final ExceptionHandler handler) {
-            new Action(consumers, handler, recursive.predicate, recursive.options.options)
-                    .walk(recursive.options.path);
-        }
-
-        public final void go() {
-            catching((path, caught) -> {
-                throw new IllegalArgumentException("Problem in <" + path + ">", caught);
-            });
+        public final <C extends Collection<? super Path>> C to(final C target) {
+            apply(target::add).run();
+            return target;
         }
     }
 
     private static class Action {
-
         private final List<Consumer> consumers;
         private final ExceptionHandler handler;
 
         private Action(final List<Consumer> consumers, final ExceptionHandler handler,
-                       final Predicate recursive, final LinkOption... options) {
+                       final Predicate recursive, final LinkOption[] options) {
             this.handler = handler;
             this.consumers = ImmutableList.<Consumer>builder()
                     .add(path -> {
